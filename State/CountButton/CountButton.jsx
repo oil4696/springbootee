6a63fa4dee01727de9b9ea2022e648db51472eb6
oil4696 <67868682+oil4696@users.noreply.github.com>
2025-06-10 19:38:50 +0900

    Delete State directory

--- State/CountButton/CountButton.jsx
+++
@@ -1,6 +0,0 @@
-function CountButton({text, value, onClick}) {
-    console.log("CountButtn 렌더링");
-    return <button value={value} onClick={onClick}>{text}</button>
-}
-
-export default CountButton;

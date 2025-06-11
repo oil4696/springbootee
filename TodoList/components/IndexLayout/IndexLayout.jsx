46ce5763d82a944d80b3d1b1b231f26d9b10fed0
oil4696 <67868682+oil4696@users.noreply.github.com>
2025-06-11 17:50:51 +0900

    Add files via upload

---
+++ TodoList/components/IndexLayout/IndexLayout.jsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import IndexHeader from '../IndexHeader/IndexHeader';
+import * as s from './styles';
+import React from 'react';
+
+function IndexLayout({children}) {
+    return (
+        <div css={s.layout}>
+             <IndexHeader />
+            {children}
+        </div>
+    );
+}
+
+export default IndexLayout;
---
+++ TodoList/components/IndexMain/IndexMain.jsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import * as s from './styles';
+import React from 'react';
+
+
+function IndexMain(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default IndexMain;
